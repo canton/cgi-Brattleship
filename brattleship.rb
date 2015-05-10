f13a84f268b444085e4d56d7672136036550d331
Ken Tong <canton1440@gmail.com>
2015-05-10 18:47:04 +0800

    submitted

--- brattleship.rb
+++ brattleship.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/env ruby
 
 def solve(r, c, w)
-  (c / w) * r + w - 1
+  last_row = (c - w - 1) / w + w + 1
+  other_rows = (c / w) * (r - 1)
+  other_rows + last_row
 end
 
 def test(r, c, w, expect)
   actual = solve r, c, w
-  raise "Q:#{r}, #{c}, #{w} expect=#{expect} actual=#{actual}"
+  raise "Q:#{r}, #{c}, #{w} expect=#{expect} actual=#{actual}" if actual != expect
 end
 
+test 1, 4, 2, 3
+test 1, 7, 7, 7
+test 2, 5, 1, 10
 test 1, 8, 3, 5
+test 3, 8, 3, 9
 test 1, 6, 4, 5
 test 1, 7, 3, 5
 
